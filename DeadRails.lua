b8db090eea94b0f6fb0c7dc2f425f78ece32ea1f
RGV-devid <youhubdoorsmobile@gmail.com>
2025-03-24 12:40:09 +0700

    Update DeadRails.lua

--- DeadRails.lua
+++ DeadRails.lua
@@ -62,6 +62,94 @@ game:GetService("RunService").RenderStepped:Connect(function()
             game:GetService("ReplicatedStorage").Remotes.Tool.PickUpTool:FireServer(unpack(args))
       end
     end
+    if Interact.AutoInteractBandage then
+      if workspace.RuntimeItems:FindFirstChild("Bandage") then
+        local args = {
+              [1] = workspace.RuntimeItems:FindFirstChild("Bandage")
+            }
+            game:GetService("ReplicatedStorage").Remotes.Tool.PickUpTool:FireServer(unpack(args))
+      end
+    end
+    if Interact.AutoInteractRevolverAmmo then
+      if workspace.RuntimeItems:FindFirstChild("RevolverAmmo") then
+        local args = {
+              [1] = workspace.RuntimeItems:FindFirstChild("RevolverAmmo")
+            }
+            game:GetService("ReplicatedStorage").Packages.RemotePromise.Remotes.C_ActivateObject:FireServer(unpack(args))
+      end
+    end
+    if Interact.AutoInteractShotgunAmmo then
+      if workspace.RuntimeItems:FindFirstChild("ShotgunShells") then
+        local args = {
+              [1] = workspace.RuntimeItems:FindFirstChild("ShotgunShells")
+            }
+            game:GetService("ReplicatedStorage").Packages.RemotePromise.Remotes.C_ActivateObject:FireServer(unpack(args))
+      end
+    end
+    if Interact.AutoInteractRifleAmmo then
+      if workspace.RuntimeItems:FindFirstChild("RifleAmmo") then
+        local args = {
+              [1] = workspace.RuntimeItems:FindFirstChild("RifleAmmo")
+            }
+            game:GetService("ReplicatedStorage").Packages.RemotePromise.Remotes.C_ActivateObject:FireServer(unpack(args))
+      end
+    end
+    if Interact.AutoInteractRevolver then
+      if workspace.RuntimeItems:FindFirstChild("Revolver") then
+        local args = {
+              [1] = workspace.RuntimeItems:FindFirstChild("Revolver")
+            }
+            game:GetService("ReplicatedStorage").Remotes.Tool.PickUpTool:FireServer(unpack(args))
+      end
+    end
+    if Interact.AutoInteractNavyRevolver then
+      if workspace.RuntimeItems:FindFirstChild("Navy Revolver") then
+        local args = {
+              [1] = workspace.RuntimeItems:FindFirstChild("Navy Revolver")
+            }
+            game:GetService("ReplicatedStorage").Remotes.Tool.PickUpTool:FireServer(unpack(args))
+      end
+    end
+    if Interact.AutoInteractMauserC96 then
+      if workspace.RuntimeItems:FindFirstChild("Mauser C96") then
+        local args = {
+              [1] = workspace.RuntimeItems:FindFirstChild("Mauser C96")
+            }
+            game:GetService("ReplicatedStorage").Remotes.Tool.PickUpTool:FireServer(unpack(args))
+      end
+    end
+    if Interact.AutoInteractShotgun then
+      if workspace.RuntimeItems:FindFirstChild("Shotgun") then
+        local args = {
+              [1] = workspace.RuntimeItems:FindFirstChild("Shotgun")
+            }
+            game:GetService("ReplicatedStorage").Remotes.Tool.PickUpTool:FireServer(unpack(args))
+      end
+    end
+    if Interact.AutoInteractRifle then
+      if workspace.RuntimeItems:FindFirstChild("Rifle") then
+        local args = {
+              [1] = workspace.RuntimeItems:FindFirstChild("Rifle")
+            }
+            game:GetService("ReplicatedStorage").Remotes.Tool.PickUpTool:FireServer(unpack(args))
+      end
+    end
+    if Interact.AutoInteractBoltActionRifle then
+      if workspace.RuntimeItems:FindFirstChild("Bolt Action Rifle") then
+        local args = {
+              [1] = workspace.RuntimeItems:FindFirstChild("Bolt Action Rifle")
+            }
+            game:GetService("ReplicatedStorage").Remotes.Tool.PickUpTool:FireServer(unpack(args))
+      end
+    end
+    if Interact.AutoInteractElectrocutioner then
+      if workspace.RuntimeItems:FindFirstChild("Electrocutioner") then
+        local args = {
+              [1] = workspace.RuntimeItems:FindFirstChild("Electrocutioner")
+            }
+            game:GetService("ReplicatedStorage").Remotes.Tool.PickUpTool:FireServer(unpack(args))
+      end
+    end
   end)
 
 
