c8a45156554327e678e5475404feef48d1674f42
RGV-devid <youhubdoorsmobile@gmail.com>
2025-03-24 11:32:03 +0700

    Update DeadRails.lua

--- DeadRails.lua
+++ DeadRails.lua
@@ -1,3 +1,5 @@
+local Entity = {"Walker","Runner","Banner","Wolf","Werewolf","Vampire","Outlaw","Unicorn","Horse"}
+
 local Interact = {
   AutoInteractMoneyBag = false,
   AutoInteractSnakeOil = false,
@@ -30,3 +32,19 @@ local Gun = {
   FastShootTime = 0,
   FastShoot = false
 }
+local Enemies = {
+  SizeHitbox = Vector3.new(0,0,0),
+  EnabledHitbox = false,
+  SizeHeadHitbox = Vector3.new(0,0,0),
+  EnabledHeadHitbox = false
+}
+local Players = {
+  SetFOV = 0,
+  FOV = false,
+  Infjump = false,
+  Noclip = false,
+  MinZoom = 0,
+  MaxZoom = 0,
+  ThirdPerson = false
+}
+  
